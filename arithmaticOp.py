d38c7f3f1ad464b8893f086790b1fc4a8b4dca25
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-05-30 22:13:48 +0530

    arithmaticOperators

---
+++ arithmaticOp.py
@@ -0,0 +1,13 @@
+a =21 
+b =10 
+print ("Addition is", a + b) 
+print ("Subtraction is ", a - b )
+print ("Multiplication is ", a * b) 
+print ("Division is ", a / b )
+print ("Modulus is ", a % b )
+a =2 
+b =3 
+print ("Power value is ", a ** b )
+a = 10 
+b = 4 
+print ("Floor Division is ", a // b )
