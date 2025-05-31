2450a1ef3bd518d7d16ed7459fd78270893d66de
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-05-31 14:04:02 +0530

    operators

---
+++ bitwiseOp.py
@@ -0,0 +1,8 @@
+a=10
+b=4
+print("Binary AND ",a&b)
+print("Binary OR ",a|b)
+print("Binary XOR",a^b)
+print("Binary Ones Complement ",~a)
+print("Binary Left Shift ",a>>2)
+print("Binary Right Shift ",a<<2)
---
+++ membershipOp.py
@@ -0,0 +1,6 @@
+a = 3 
+list = [1, 2, 3, 4, 5 ]; 
+if ( a in list ):
+    print ("available") 
+else:
+    print (" not available")
