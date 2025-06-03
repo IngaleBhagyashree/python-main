5cc926924524ae058c71d3450baf018951986214
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-03 14:54:43 +0530

    decisionLoop_for

---
+++ decisionLoop_For.py
@@ -0,0 +1,11 @@
+for i in range (1,4):
+    print(i)
+print("end")
+
+for i in range (10, 0, -1):
+    print (i, end=" ")
+print("end")
+    
+name ="PYTHON"
+for letter in name:
+    print(letter, end=" ")
