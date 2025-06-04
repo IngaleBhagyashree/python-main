5eb0e6d12c799578ffa842a73cf2a739c9516607
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-04 15:29:26 +0530

    StarPattern_CSE

---
+++ starPattern6.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    for j in range(1,4):
+        if i==1 or i==3 or i==5:
+            print("*",end=" ")
+        elif i==2 and j==1:
+            print("*",end=" ")
+        elif i==4 and j==3:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print(" ")
