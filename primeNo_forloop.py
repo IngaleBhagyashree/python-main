62e707aba35ca98d57d6716c353d108bde541ce8
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-05 13:24:16 +0530

    primeNo_Forloop

---
+++ primeNo_forloop.py
@@ -0,0 +1,10 @@
+n=int(input("enter number:"))
+count=0
+for i in range (2,n):
+    if n%i==0:
+        count=count+1
+        break
+if count==0:
+    print("number is prime ")
+else:
+    print("number is not prime")
