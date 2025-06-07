e58e90426cd0aa0b25bdca3747f2cd7ca2e2bca6
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-07 19:00:31 +0530

    function

---
+++ function.py
@@ -0,0 +1,17 @@
+# def functionname (parameters): 
+#     """function_docstring""" 
+#     function_suite 
+#     return [expression] 
+
+def add(a,b): 
+    """This function sum the numbers""" 
+    c=a+b 
+    print (c) 
+add(5,12) # 17 
+add(1,5.6)#6.6
+
+# def add(a,b): 
+#     """This function sum thenumbers""" 
+#     c=a+b 
+#     print (c) 
+#     return 
