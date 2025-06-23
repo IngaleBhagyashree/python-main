eb1591f9dc59e3081349d0cbcfbf62bbe658c6b9
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-23 20:51:00 +0530

    local Global variable

---
+++ localGlobalVariable.py
@@ -0,0 +1,8 @@
+a=11 
+def myfunction(): 
+    b=10 
+    print ("Inside function",a) #display global var 
+    print ("Inside function",b) #display local var 
+myfunction() 
+print ("outside function",a) # available print 
+print ("outside function",b) # error
