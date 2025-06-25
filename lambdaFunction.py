4b8312ee92f6b1a3d708f80701d6b132726bed20
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-25 15:06:22 +0530

    lambda function

---
+++ lambdaFunction.py
@@ -0,0 +1,7 @@
+# def square(x): 
+#     return x*x 
+# the same function can be written as anonymous function as: 
+f=lambda x: x*x 
+value=f(5)
+print(value)
+# The colon (:) represents the beginning of the function that contains an expression x*x. 
