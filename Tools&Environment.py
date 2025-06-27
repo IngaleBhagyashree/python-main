dd85593b4d19deb1f6e6506375cd6a3fa48ce2f7
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-06-27 16:06:25 +0530

    git and env creation

---
+++ Tools&Environment.py
@@ -0,0 +1,8 @@
+# Explanation: Track code changes and collaborate
+# git init
+# git commit -m "Initial commit"
+
+#  Virtual Environment
+# Explanation: Isolate project dependencies.
+# python -m venv env
+# .\env\Scripts\activate on Windows
