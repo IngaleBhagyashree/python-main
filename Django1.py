14cdb4fe3a050f4e91e91dd43b6f7432f316d568
IngaleBhagyashree <ingalebhagyashree7@gmail.com>
2025-07-01 12:11:30 +0530

    django initial

---
+++ Django1.py
@@ -0,0 +1,14 @@
+# What You Can Build with Django:
+# Blog websites
+# E-commerce platforms
+# Portfolio sites
+# Social networks
+# APIs (using Django REST Framework)
+
+
+from django.db import models
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+
+# Django is a high-level Python web framework that enables developers to build robust, secure, and scalable web applications quickly and with clean, reusable code.
+
